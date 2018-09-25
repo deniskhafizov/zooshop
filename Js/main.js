f880551f543ca56fca2900f1e7e61fedeac66d1c
denis <534355@mail.ru>
2018-09-25 15:17:50 +0300

    Исправлен js для меню навигации.

--- Js/main.js
+++ Js/main.js
@@ -18,20 +18,14 @@ const siteNames = {
     "order": "Форма заказа",
     "feedback": "Форма обратной связи",
     "contacts": "Наши контакты",
+    "default": "Добро пожаловать!"
 };
 
 function getMenu() {
-    const url = document.location.pathname;
-    for (let key in menu) {
-        if (url.includes(key)){
-            menu = menues [key]
-            console.log("Соответствие найдено. Устанавливаем имя страницы: " + menu);
-            break
-        }
-    }
+    const menu = '<a href="index.html"> ГЛАВНАЯ </a> <a href="about.html">ОПИСАНИЕ КОМПАНИИ</a>' +
+        '<a href="friends.html">ДОМАШНИЙ ДРУГ</a> <a href="order.html">ФОРМА ЗАКАЗА</a> ' +
+        '<a href="feedback.html">ОБРАТНАЯ СВЯЗЬ</a> <a href="contacts.html">КОНТАКТЫ</a>'
+    console.log (menu)
     return menu;
 }
 
-const menu = '<a href="index.html"> Главная </a> <a href="about.html">ОПИСАНИЕ КОМПАНИИ</a>' +
-    '<a href="friends.html">ДОМАШНИЙ ДРУГ</a> <a href="order.html">ФОРМА ЗАКАЗА</a> ' +
-    '<a href="feedback.html">ОБРАТНАЯ СВЯЗЬ</a> <a href="contacts.html">КОНТАКТЫ</a>'
